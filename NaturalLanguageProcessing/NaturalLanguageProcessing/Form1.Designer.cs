534babc2484ba28d60af7f0c2524f14726ffd199
fulyaerr <fulyaer.fe22@gmail.com>
2022-02-10 20:40:56 +0300

    Add files via upload

---
+++ NaturalLanguageProcessing/NaturalLanguageProcessing/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace NaturalLanguageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonTokenize = new System.Windows.Forms.Button();
+            this.txtTokenize = new System.Windows.Forms.TextBox();
+            this.txtRoot = new System.Windows.Forms.TextBox();
+            this.btnFindRoot = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDosyaSec = new System.Windows.Forms.Button();
+            this.btnNormalize = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(680, 182);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(156, 46);
+            this.buttonClear.TabIndex = 0;
+            this.buttonClear.Text = "Veri Temizleme";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 150);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonTokenize
+            // 
+            this.buttonTokenize.Location = new System.Drawing.Point(856, 26);
+            this.buttonTokenize.Name = "buttonTokenize";
+            this.buttonTokenize.Size = new System.Drawing.Size(228, 46);
+            this.buttonTokenize.TabIndex = 2;
+            this.buttonTokenize.Text = "Tokenize";
+            this.buttonTokenize.UseVisualStyleBackColor = true;
+            this.buttonTokenize.Click += new System.EventHandler(this.buttonTokenize_Click);
+            // 
+            // txtTokenize
+            // 
+            this.txtTokenize.Location = new System.Drawing.Point(856, 78);
+            this.txtTokenize.Multiline = true;
+            this.txtTokenize.Name = "txtTokenize";
+            this.txtTokenize.Size = new System.Drawing.Size(228, 514);
+            this.txtTokenize.TabIndex = 3;
+            // 
+            // txtRoot
+            // 
+            this.txtRoot.Location = new System.Drawing.Point(60, 234);
+            this.txtRoot.Multiline = true;
+            this.txtRoot.Name = "txtRoot";
+            this.txtRoot.Size = new System.Drawing.Size(776, 150);
+            this.txtRoot.TabIndex = 4;
+            // 
+            // btnFindRoot
+            // 
+            this.btnFindRoot.Location = new System.Drawing.Point(60, 390);
+            this.btnFindRoot.Name = "btnFindRoot";
+            this.btnFindRoot.Size = new System.Drawing.Size(156, 46);
+            this.btnFindRoot.TabIndex = 5;
+            this.btnFindRoot.Text = "Kökleri Bul";
+            this.btnFindRoot.UseVisualStyleBackColor = true;
+            this.btnFindRoot.Click += new System.EventHandler(this.btnFindRoot_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(60, 442);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(776, 150);
+            this.textBox3.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1109, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(630, 514);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // btnDosyaSec
+            // 
+            this.btnDosyaSec.Location = new System.Drawing.Point(1109, 26);
+            this.btnDosyaSec.Name = "btnDosyaSec";
+            this.btnDosyaSec.Size = new System.Drawing.Size(228, 46);
+            this.btnDosyaSec.TabIndex = 8;
+            this.btnDosyaSec.Text = "Dosya Seç";
+            this.btnDosyaSec.UseVisualStyleBackColor = true;
+            this.btnDosyaSec.Click += new System.EventHandler(this.btnDosyaSec_Click);
+            // 
+            // btnNormalize
+            // 
+            this.btnNormalize.Location = new System.Drawing.Point(507, 182);
+            this.btnNormalize.Name = "btnNormalize";
+            this.btnNormalize.Size = new System.Drawing.Size(156, 46);
+            this.btnNormalize.TabIndex = 9;
+            this.btnNormalize.Text = "Normalize Et";
+            this.btnNormalize.UseVisualStyleBackColor = true;
+            this.btnNormalize.Click += new System.EventHandler(this.btnNormalize_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1761, 639);
+            this.Controls.Add(this.btnNormalize);
+            this.Controls.Add(this.btnDosyaSec);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnFindRoot);
+            this.Controls.Add(this.txtRoot);
+            this.Controls.Add(this.txtTokenize);
+            this.Controls.Add(this.buttonTokenize);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonTokenize;
+        private System.Windows.Forms.TextBox txtTokenize;
+        private System.Windows.Forms.TextBox txtRoot;
+        private System.Windows.Forms.Button btnFindRoot;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDosyaSec;
+        private System.Windows.Forms.Button btnNormalize;
+    }
+}
+
---
+++ NaturalLanguageProcessing/NaturalLanguageProcessing/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace NaturalLanguageProcessing
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<char, char> characterList = new Dictionary<char, char>();
+        public Form1()
+        {
+            InitializeComponent();
+            characterList = new Dictionary<char, char>() { { 'b', 'p' }, { 'c', 'ç' }, { 'd', 't' }, { 'g', 'k' }, { 'ğ', 'k' } };
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            string data = string.Empty;
+
+            Regex rgx = new Regex(@"[^\w\d]");
+
+            data = rgx.Replace(textBox1.Text, " ");
+            while (data.Contains("  "))
+            {
+                data = data.Replace("  ", " ");
+            }
+
+            textBox1.Text = data.ToLower();
+            textBox1.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(textBox1.Text);
+        }
+
+        private void buttonTokenize_Click(object sender, EventArgs e)
+        {
+            string[] words = textBox1.Text.Split(' ');
+            txtTokenize.Lines = words.ToArray();
+        }
+
+        private void btnFindRoot_Click(object sender, EventArgs e)
+        {
+            List<RowDetail> rowList = new List<RowDetail>();
+            List<StemDetail> stemList = new List<StemDetail>();
+            int flag = 0;
+            string searched_word = string.Empty;
+            string variant = string.Empty;
+            int stemCount = 1;
+
+            for (int k = 0; k < dataGridView1.Columns["Key"].DataGridView.Rows.Count - 1; k++)
+            {
+                rowList.Add(
+                    new RowDetail
+                    {
+                        key = dataGridView1.Rows[k].Cells["Key"].Value.ToString(),
+                        tag = dataGridView1.Rows[k].Cells["Tag"].Value.ToString()
+                    });
+            }
+
+            for (int i = 0; i < txtTokenize.Lines.Length; i++)
+            {
+                flag = 0;
+                searched_word = txtTokenize.Lines[i];
+                string stem = string.Empty;
+                string key = string.Empty;
+                while ((flag == 0) && searched_word.Length > 1)
+                {
+                    variant = string.Empty;
+                    var endOfWord = searched_word.ToCharArray();
+                    var lastCharacter = endOfWord[endOfWord.Length - 1];
+
+                    if (characterList.Keys.Contains(lastCharacter))
+                    {
+                        characterList.TryGetValue(lastCharacter, out lastCharacter);
+                    }
+
+                    variant = searched_word.Substring(0, searched_word.Length - 1) + lastCharacter;
+
+                    if (rowList.Where(x => x.key == variant.Trim()).Any() || rowList.Where(x => x.key == searched_word.Trim()).Any())
+                    {
+                        key = rowList.Where(x => x.key == variant.Trim()).Any() ? variant.Trim() : searched_word.Trim();
+                        stem = rowList.Find(x => x.key == key).tag;
+                        if (!stemList.Where(x => x.stem == key).Any())
+                        {
+                            stemList.Add(new StemDetail
+                            {
+                                id = stemCount,
+                                stem = key,
+                                stem_type = stem
+                            });
+
+                            stemCount++;
+                        }
+                        flag = 1;
+                    }
+                    else
+                    {
+                        searched_word = searched_word.Substring(0, searched_word.Length - 1);
+                    }
+                }
+
+                if (flag == 1)
+                {
+                    textBox3.Text = textBox3.Text + (" " + key + "[" + stem + "] ");
+                }
+                else
+                {
+                    textBox3.Text = textBox3.Text + " " + txtTokenize.Lines[i];
+                }
+            }
+        }
+        private void btnDosyaSec_Click(object sender, EventArgs e)
+        {
+            string DosyaYolu;
+            string DosyaAdi;
+            DataTable dt;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;
+                DosyaAdi = file.SafeFileName;
+                ExcelApp.Application excelApp = new ExcelApp.Application();
+                if (excelApp == null)
+                { 
+                    MessageBox.Show("Excel yüklü değil.");
+                    return;
+                }
+                ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(DosyaYolu);
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+                int satirSayisi = excelRange.Rows.Count; 
+                int sutunSayisi = excelRange.Columns.Count;
+                dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+            else
+            {
+                MessageBox.Show("Dosya Seçilemedi.");
+            }
+        }
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { 
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else
+                            table.Columns.Add(j.ToString() + ".Sütun");
+                    }
+                    continue;
+                }
+               
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    else
+                        yeniSatir[j - 1] = String.Empty;
+                }
+                table.Rows.Add(yeniSatir);
+            }
+            return table;
+        }
+        public class StemDetail
+        {
+            public int id { get; set; }
+            public string stem { get; set; }
+            public string stem_type { get; set; }
+        }
+
+        public class RowDetail
+        {
+            public string key { get; set; }
+            public string tag { get; set; }
+        }
+
+    }
+}
